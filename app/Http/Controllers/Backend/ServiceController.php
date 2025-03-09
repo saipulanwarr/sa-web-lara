e5b0edb9e161580904155dec451ff41123ab565f
saipulanwarr <saipulanwar180994@gmail.com>
2025-03-09 21:18:03 +0700

    added update service

--- app/Http/Controllers/Backend/ServiceController.php
+++ app/Http/Controllers/Backend/ServiceController.php
@@ -19,6 +19,13 @@ class ServiceController extends Controller
         return view('backend.service.add_service');
     }
 
+    private function deleteOldImage(string $oldPhotoPath): void{
+        $fullPath = public_path('upload/service/'.$oldPhotoPath);
+        if(file_exists($fullPath)){
+           unlink($fullPath);
+        }
+    }
+
     public function StoreService(Request $request){
 
         if($request->file('image')){
@@ -42,5 +49,59 @@ class ServiceController extends Controller
         );
 
         return redirect()->route('all.service')->with($notification);
+    }
+
+    public function EditService($id){
+        $service = Service::find($id);
+
+        return view('backend.service.edit_service', compact('service'));
+    }
+
+    public function updateService(Request $request){
+        $service_id = $request->id;
+        $service = Service::find($service_id);
+
+        if($request->file('image')){
+            $oldPhotoPath = $service->image;
+            $image = $request->file('image');
+            $filename = time().'.'.$image->getClientOriginalExtension();
+            $image->move(public_path('upload/service'), $filename);
+
+            $service->update([
+               'service_name' => $request->service_name,
+                'slug' => strtolower(str_replace(' ', '-', $request->service_name)),
+                'service_short' => $request->service_short,
+                'service_desc' => $request->service_desc,
+                'icon' => $request->icon,
+                'image' => $filename,
+            ]);
+
+            if($oldPhotoPath && $oldPhotoPath !== $filename){
+                $this->deleteOldImage($oldPhotoPath);
+             }
+
+            $notification = array(
+                'message' => 'Service Updated with Image Successfully',
+                'alert-type' => 'success'
+            );
+    
+            return redirect()->route('all.service')->with($notification);
+
+        }else{
+            $service->update([
+                'service_name' => $request->service_name,
+                'slug' => strtolower(str_replace(' ', '-', $request->service_name)),
+                'service_short' => $request->service_short,
+                'service_desc' => $request->service_desc,
+                'icon' => $request->icon,
+            ]);
+
+            $notification = array(
+                'message' => 'Service Updated without Image Successfully',
+                'alert-type' => 'success'
+            );
+    
+            return redirect()->route('all.service')->with($notification);
+        }
     }
 }
